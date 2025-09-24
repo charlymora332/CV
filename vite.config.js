be80d784dbfe33c4c302cb240f76b444dd19e6b9
Carlos Mora <charlymora332@gmail.com>
2025-09-24 09:25:26 -0500

    correcion ruta

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: 'https://charlymora332.github.io/CV',
 });
